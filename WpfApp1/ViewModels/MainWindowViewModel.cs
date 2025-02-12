76d570d6f6ff064a3ba3003e9b9a86a4fa787069
ReoF777 <reonafuji@gmail.com>
2025-02-12 23:40:59 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+namespace WpfApp1.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private int? _num = 0;
+
+        [ObservableProperty]
+        private string? _name = "Hello World";
+
+        [RelayCommand]
+        public void Add () {
+            Num++;
+        }
+    }
+}
