34c21b65136ade40fc99759b9b6a92b979645464
ReoF777 <reonafuji@gmail.com>
2025-02-14 11:18:58 +0900

    一定時間ごとにmodel側からの更新を行う処理を実装

---
+++ WpfApp1/Models/Model.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+namespace WpfApp1.Models
+{
+    public partial class Model : ObservableObject
+    {
+        [ObservableProperty]
+        private int _num = 0;
+
+        public Model () {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await Task.Delay(1000);
+                    Num++;
+                }
+            });
+        }
+    }
+}
--- WpfApp1/ViewModels/MainWindowViewModel.cs
+++ WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,24 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using WpfApp1.Models;
 namespace WpfApp1.ViewModels
 {
     public partial class MainWindowViewModel : ObservableObject
     {
+        private readonly Model _model;
+
+        public MainWindowViewModel () {
+            _model = new Model();
+
+            _model.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == nameof(_model.Num))
+                {
+                    Num = _model.Num;
+                }
+            };
+        }
+
         [ObservableProperty]
         private int? _num = 0;
 
